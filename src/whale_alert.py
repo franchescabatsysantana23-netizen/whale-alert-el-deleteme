c4459874d194d4449d8c40af768f94e07076ae31
Franchesca Batsy Santana Liriano <franchescabatsysantana23@gmail.com>
2026-06-09 17:20:02 -0400

    se agrega liberias

---
+++ src/whale_alert.py
@@ -0,0 +1,16 @@
+'''
+1. requests al whale alert
+2. utilizar el beautifulsoup para extraer la informacion. 
+3. parsear la data que me retorna el response
+4. utilizar pandas para transformar la data en un dataframe.
+5.cargar el dataframe a un archivo csv. 
+6. subir el archivo a un bucket de minio. 
+
+'''
+import requests 
+from bs4 import BeautifulSoup 
+import pandas as pd
+
+url="https://whale-alert.io/whales.html"
+response=requests.get(url)
+print(response)

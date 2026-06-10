19aec49295e3588b7ab3b3a1287fb50e29eed8cd
Franchesca Batsy Santana Liriano <franchescabatsysantana23@gmail.com>
2026-06-10 18:53:11 -0400

    refactor en el codigo

--- src/whale_alert.py
+++ src/whale_alert.py
@@ -10,12 +10,58 @@
 import requests 
 from bs4 import BeautifulSoup 
 import pandas as pd
+from datetime import datetime,timezone
+from logging import Logger,FileHandler,DEBUG
 
-url="https://whale-alert.io/whales.html"
-response=requests.get(url)
-response.encoding='utf-8'
 
-soup=BeautifulSoup(response.content,'html.parser')
-table =soup.select('table.table tbody tr')
 
-print(table)
+def whale_alert_extractor() -> dict[str,list[str]] | None:
+    
+    logger =Logger(whale_alert_extractor)
+    file_handler= FileHandler('logs.log')
+    logger.setLevel(DEBUG)
+    logger.info('iniciando')
+    logger.addHandler(file_handler)
+    logger.info('agregando handler')
+    url="https://whale-alert.io/whales.html"
+    try:
+
+    
+        response=requests.get(url)
+        logger.debug(f'Obteniendo datos  de la url: {url}')
+        response.encoding='utf-8'
+
+        soup=BeautifulSoup(response.content,'html.parser')
+        table_rows =soup.select('table.table tbody tr')
+
+        data ={
+        "datetime_utc":[datetime.now(timezone.utc)] * len(table_rows),
+        "crypto":[
+        (row.find("th",{"scope":"row"}).find("img")["alt"].strip()
+        if row.find('th',{"scope":"row"}).find("img")
+        else row.find("th",{"scope":"row"}).get_text(strip=True))
+        for row in table_rows ],
+        "known":[row.find_all("td")[0].get_text(strip=True) for row in table_rows],
+        "unknown":[row.find_all('td')[1].get_text(strip=True) for row in table_rows]
+        }
+        logger.info(f'Datos obtenidos: {len(data)} filas.')
+
+        return data
+        """
+              whale_alert_df=pd.DataFrame(data) # crea el dataframe.
+              print(whale_alert_df.head(10)) # muestra los primeros 10 registros.
+              whale_alert_df.to_csv("whale_alert.csv",index=False) # carga el dataframe a un archivo csv. 
+        """
+      
+    except Exception as e:
+        logger.error(f'error {str(e)}')
+        return None
+
+        
+whale_alert_df =whale_alert_extractor()  
+if whale_alert_df is not None:
+    whale_alert_df1=pd.DataFrame(whale_alert_df)
+    print(whale_alert_df1.head(10))  
+else:
+    print('no se pudo obtener los datos')
+    
